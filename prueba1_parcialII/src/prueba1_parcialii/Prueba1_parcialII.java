1ae6293d7cba3937f34bdef20f3f58b24da23853
alira <aliraquel95@gmail.com>
2026-05-28 15:09:16 -0600

    m

---
+++ prueba1_parcialII/src/prueba1_parcialii/Prueba1_parcialII.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prueba1_parcialii;
+
+/**
+ *
+ * @author alira
+ */
+public class Prueba1_parcialII {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
